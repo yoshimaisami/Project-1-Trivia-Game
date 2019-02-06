16cee03719a10fe5be4601ad19e45f8867de3c9e
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-02-06 12:16:52 -0500

    made progress with html and js, need to revist css flex and grid

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Project-1-Trivia-Game
+
 Trivia Game to teach kids how to Read.
+
+create github Project 1 repo on personal account
+clone to local machine
+create html, css, and js files
+
+1. setup the boilder plate
+2. sketch design
+3. outline HTML
+
+body
+header
+h1
+p1
+h2
+score???
+main
+image
+h3 question #
+section
+<a .correctAnswer
+<a .incorrectAnswer
+footer
+<a .nextQuestion
+<a .resetGame
--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Hello World");
+const questions = [
+  {
+    challenge: 0,
+    question:
+      "https://image.freepik.com/free-vector/cute-dog-breeds_23-2147519661.jpg", //insertImage1dog,
+    option: ["Dog", "Cat"],
+    // option: [{
+    //     en: 'Dog',
+    //     sp: 'Perro'
+    // },
+    // {
+    //     en:'Cat',
+    //     sp: 'Gato'
+    // }
+    // ],
+    correctAnswer: "Dog"
+    // correctAnswerIndex: 0
+  },
+
+  {
+    challenge: 1,
+    question:
+      "https://www.freepik.com/free-vector/beautiful-frog-emoticons-set_1020311.htm", //insertImagefrog,
+    options: ["Frog", "Bird"],
+    correctAnswer: "Frog"
+  },
+
+  {
+    challenge: 2,
+    question:
+      "https://image.freepik.com/free-vector/flat-easter-bunny-collection_23-2147752672.jpg", //insertImage3bunny,
+    options: ["Bunny", "Horse"],
+    correctAnswer: "Bunny"
+  }
+];
+
+/* Notes: 
+need to display "question" image in question div
+display corresponding options beneath the question image
+user should be able to "click" on options
+system will check for correctAnswer
+if correct, show congrats msg (bonus: play matching animal sound "woof-woof")
+if wrong, show try again msg (bonus: play whoopsi)
+
+Questions:
+should I split the options array up, and have two seperate keys...correctAnswer & incorrectAnswer...might make it easier for validation.
+
+
+*/
