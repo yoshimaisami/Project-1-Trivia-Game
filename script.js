e9bf2b381916fac5cbca89048e4888022287ae5c
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-02-07 10:20:28 -0500

    fixed grid in css and html, should help with dom manipulation too

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-// const challenges = [
-//   {
-//     // question:
-//     // "https://image.freepik.com/free-vector/cute-dog-breeds_23-2147519661.jpg", //insertImage1dog,
-//     option: ["Dog", "Cat"],
-//     correctAnswer: "Dog"
-//     // correctAnswerIndex: 0
-//   },
+const challenges = [
+  {
+    // question:
+    // "https://image.freepik.com/free-vector/cute-dog-breeds_23-2147519661.jpg", //insertImage1dog,
+    option: ["Dog", "Cat"],
+    correctAnswer: "Dog"
+    // correctAnswerIndex: 0
+  },
 
-//   {
-//     question:
-//       "https://image.freepik.com/free-vector/beautiful-frog-emoticons-set_23-2147592374.jpg", //insertImagefrog,
-//     options: ["Frog", "Bird"],
-//     correctAnswer: "Frog"
-//   },
+  {
+    question:
+      "https://image.freepik.com/free-vector/beautiful-frog-emoticons-set_23-2147592374.jpg", //insertImagefrog,
+    options: ["Frog", "Bird"],
+    correctAnswer: "Frog"
+  },
 
-//   {
-//     src: "http://place-sloth.com/500-500.png"
-//   },
+  {
+    src: "http://place-sloth.com/500-500.png"
+  },
 
-//   {
-//     question:
-//       "https://image.freepik.com/free-vector/flat-easter-bunny-collection_23-2147752672.jpg", //insertImage3bunny,
-//     options: ["Bunny", "Horse"],
-//     correctAnswer: "Bunny"
-//   }
-// ];
-console.log("Hello");
+  {
+    question:
+      "https://image.freepik.com/free-vector/flat-easter-bunny-collection_23-2147752672.jpg", //insertImage3bunny,
+    options: ["Bunny", "Horse"],
+    correctAnswer: "Bunny"
+  }
+];
 
-document.getElementsByClassName("question").innerHTML = "<p>Hello</p>";
+document.getElementsByClassName("questionImage").innerHTML = "<p>Hello</p>";
 
-// optionName.innerText = "Hello";
+// console.log("Hello");
+
+// document.getElementsByClassName("question").innerHTML = "<p>Hello</p>";
+
+// // optionName.innerText = "Hello";
 // console.log(optionName.innerHTML);
 
 // let picture = document.getElementsByClassName("question");
@@ -54,15 +57,13 @@ document.getElementsByClassName("question").innerHTML = "<p>Hello</p>";
 //challenges[0].option[0]; //dog
 //challenges[0].option[1]; //cat
 
-/* Notes: 
-need to display "question" image in question div
-display corresponding options beneath the question image
-user should be able to "click" on options
-system will check for correctAnswer
-if correct, show congrats msg (bonus: play matching animal sound "woof-woof")
-if wrong, show try again msg (bonus: play whoopsi)
+// Notes:
+// need to display "question" image in question div
+// display corresponding options beneath the question image
+// user should be able to "click" on options
+// system will check for correctAnswer
+// if correct, show congrats msg (bonus: play matching animal sound "woof-woof")
+// if wrong, show try again msg (bonus: play whoopsi)
 
-Questions:
-should I split the options array up, and have two seperate keys...correctAnswer & incorrectAnswer...might make it easier for validation.
-
-*/
+// Questions:
+// should I split the options array up, and have two seperate keys...correctAnswer & incorrectAnswer...might make it easier for validation.
